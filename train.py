20f458095b40b45614e78be1c47a3d3c7f623b29
Tom Gotsman <64492814+tgberkeley@users.noreply.github.com>
2021-07-06 21:03:57 +0100

    Update train.py

--- train.py
+++ train.py
@@ -77,7 +77,7 @@ transform_image_shape_no_flip = DataAugmentor(image_size, image_size)
 
 
 print('Loading the data')
-train_dataset_no_flip = AffectNet(root_path='/vol/bitbucket/tg220/data/AffectNet_val_set/', subset='val', n_expression=n_expression,
+train_dataset_no_flip = AffectNet(root_path='/vol/bitbucket/tg220/data/train_set/', subset='val', n_expression=n_expression,
                          transform_image_shape=transform_image_shape_no_flip, transform_image=transform_image)
 
 test_dataset_no_flip = AffectNet(root_path='/vol/bitbucket/tg220/data/AffectNet_val_set/', subset='test', n_expression=n_expression,
