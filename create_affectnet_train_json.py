2238b659b88166907a5a562e3c6c454c18ff3f90
Tom Gotsman <64492814+tgberkeley@users.noreply.github.com>
2021-07-09 16:06:51 +0100

    Update create_affectnet_train_json.py

--- create_affectnet_train_json.py
+++ create_affectnet_train_json.py
@@ -46,7 +46,9 @@ for file in all_files:
     image_file = str(root_path) + "/images/" + file
 
     image = io.imread(image_file)
-    image = np.ascontiguousarray(image).to(device)
+    image = np.ascontiguousarray(image)
+    transform_image = transforms.Compose([transforms.ToTensor()])
+    image = transform_image(image).to(device)
     predicted_landmarks = fa.get_landmarks(image)
     predicted_landmarks = np.array(predicted_landmarks).squeeze()
     predicted_landmarks = predicted_landmarks.tolist()
