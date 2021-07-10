e9bd3647132b26616d550319b31b71faad0f3e8d
Tom Gotsman <tomgotsman1@yahoo.co.uk>
2021-07-10 10:15:18 +0100

    update with ratio

--- AffectNet_dataloader.py
+++ AffectNet_dataloader.py
@@ -15,6 +15,7 @@ import imutils
 import face_alignment
 from skimage import io
 from Face_Occlusion import VR_patch
+import json
 
 
 #### to run some tests on the landmarks
@@ -40,7 +41,7 @@ fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cpu'
 
 class AffectNet(Dataset):
 
-    def __init__(self, root_path, subset='test',
+    def __init__(self, root_path, subset='train',
                  transform_image_shape=None, transform_image=None,
                  n_expression=5, verbose=1, cleaned_set=True):
         self.root_path = Path(root_path).expanduser()
@@ -49,39 +50,52 @@ class AffectNet(Dataset):
         self.transform_image = transform_image
         self.verbose = verbose
 
-        all_data = {}
+        # all_data = {}
+        #
+        # all_files = os.listdir(self.root_path.joinpath('images/'))
+        #
+        #
+        # if ".DS_Store" in all_files:
+        #     all_files.remove(".DS_Store")
+        #
+        #
+        # for file in all_files:
+        #     image_name = os.path.splitext(file)[0]
+        #
+        #     file_path = str(self.root_path) + "/annotations/" + image_name
+        #
+        #     arousal = np.load(file_path + '_aro.npy')
+        #     valence = np.load(file_path + '_val.npy')
+        #     expression = np.load(file_path + '_exp.npy')
+        #
+        #     landmarks = np.load(file_path + '_lnd.npy')
+        #     landmarks = np.reshape(landmarks, (-1, 2))
+        #
+        #     image_file = str(self.root_path) + "/images/" + file
+        #
+        #     image = io.imread(image_file)
+        #     image = np.ascontiguousarray(image)
+        #     predicted_landmarks = fa.get_landmarks(image)
+        #     predicted_landmarks = np.array(predicted_landmarks).squeeze()
+        #
+        #     all_data[image_name] = {'valence': valence.item(),
+        #                             'arousal': arousal.item(),
+        #                             'expression': expression.item(),
+        #                             'gt_landmarks': landmarks,
+        #                             'my_landmarks': predicted_landmarks}
+        #
+        #
 
-        all_files = os.listdir(self.root_path.joinpath('images/'))
+        if subset == 'train':
+            file = '/vol/bitbucket/tg220/data/training_data_affectnet.json'
+
+        if subset == 'test':
+            file = '/vol/bitbucket/tg220/data/affectnet_val_data.json'
+            #file = 'affectnet_val_data.json'
 
 
-        if ".DS_Store" in all_files:
-            all_files.remove(".DS_Store")
-
-
-        for file in all_files:
-            image_name = os.path.splitext(file)[0]
-
-            file_path = str(self.root_path) + "/annotations/" + image_name
-
-            arousal = np.load(file_path + '_aro.npy')
-            valence = np.load(file_path + '_val.npy')
-            expression = np.load(file_path + '_exp.npy')
-
-            landmarks = np.load(file_path + '_lnd.npy')
-            landmarks = np.reshape(landmarks, (-1, 2))
-
-            image_file = str(self.root_path) + "/images/" + file
-
-            image = io.imread(image_file)
-            image = np.ascontiguousarray(image)
-            predicted_landmarks = fa.get_landmarks(image)
-            predicted_landmarks = np.array(predicted_landmarks).squeeze()
-
-            all_data[image_name] = {'valence': valence.item(),
-                                    'arousal': arousal.item(),
-                                    'expression': expression.item(),
-                                    'gt_landmarks': landmarks,
-                                    'my_landmarks': predicted_landmarks}
+        with open(file) as read_file:
+            all_data = json.load(read_file)
 
         print(len(all_data.keys()))
 
@@ -91,6 +105,16 @@ class AffectNet(Dataset):
 
         self.frame_keys_subset = []
 
+        # 0:Neutral 1:Happy 2:Sad 3:Surprise 4:Fear 5:Disgust 6:Anger 7:Contempt
+        # (looking at the VA cirlce we really dont cover the bottom half very well at all)
+        self.expr_to_VA = {0: {'valence': 0, 'arousal': 0},
+                      1: {'valence': 0.9, 'arousal': 0.16},
+                      2: {'valence': -0.81, 'arousal': -0.4},
+                      3: {'valence': 0.42, 'arousal': 0.88},
+                      4: {'valence': -0.11, 'arousal': 0.79},
+                      5: {'valence': -0.67, 'arousal': 0.49},
+                      6: {'valence': -0.41, 'arousal': 0.78},
+                      7: {'valence': -0.57, 'arousal': 0.66}}
 
        # self.frame_keys_subset = self.image_keys[130:210]
        # print(len(self.frame_keys_subset))
@@ -100,11 +124,11 @@ class AffectNet(Dataset):
         # return len(self.frame_keys)
 
     def __getitem__(self, index):
-        # here index refers to the image code eg: '338/f60cd0dabca4c9cfcf2649cc99934d2570bddfd491d4420dac98bf49.jpg'
+
         ignore_bounding_box = False
         bounding_box = None
         key = self.image_keys[index]
-        print(key)
+        #print(key)
 
         sample_image = self.all_data[key]
 
@@ -118,6 +142,10 @@ class AffectNet(Dataset):
         arousal = torch.tensor([float(sample_image['arousal'])], dtype=torch.float32)
         expression = torch.tensor([float(sample_image['expression'])], dtype=torch.float32)
 
+        val_from_expr = torch.tensor([float(self.expr_to_VA[int(expression)]['valence'])], dtype=torch.float32)
+        aro_from_expr = torch.tensor([float(self.expr_to_VA[int(expression)]['arousal'])], dtype=torch.float32)
+
+
         if valence == -2:
             valence = 0
         if arousal == -2:
@@ -126,6 +154,7 @@ class AffectNet(Dataset):
         gt_landmarks = sample_image['gt_landmarks']
         predicted_landmarks = sample_image['my_landmarks']
 
+        predicted_landmarks = np.array(predicted_landmarks)
 
         if len(predicted_landmarks.shape) > 2:
             predicted_landmarks = predicted_landmarks[1,:,:]
@@ -166,7 +195,10 @@ class AffectNet(Dataset):
             image = self.transform_image(image)
 
 
-        return dict(valence=valence, arousal=arousal, expression=1, image=image, au=[])
+
+        return dict(valence=valence, arousal=arousal, expression=expression, val_from_expr=val_from_expr,
+                    aro_from_expr=aro_from_expr, image=image, au=[])
+
 
 
 
