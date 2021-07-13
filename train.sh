7dda94e1afb3bf2bc42c7ea45dda22ade65389b0
Tom Gotsman <64492814+tgberkeley@users.noreply.github.com>
2021-07-13 19:42:37 +0100

    Update train.sh

--- train.sh
+++ train.sh
@@ -3,6 +3,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL # required to send email notifcations
 #SBATCH --mail-user=tg220 # required to send email notifcations - please replace <your_username> with your college login name or email address
+#SBATCH --gres=gpu:2
 #SBATCH --output=/vol/bitbucket/tg220/results/out_test_practice%j.out
 export PYTHONPATH=/vol/bitbucket/tg220/emotiondetector/emonet_master
 export PATH=/vol/bitbucket/tg220/miniconda3/bin/:$PATH
