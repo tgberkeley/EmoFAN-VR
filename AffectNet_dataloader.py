932a60cead483a49a34fe502752d4ea057060b9d
Tom Gotsman <64492814+tgberkeley@users.noreply.github.com>
2021-08-30 17:10:43 +0100

    Update AffectNet_dataloader.py

--- AffectNet_dataloader.py
+++ AffectNet_dataloader.py
@@ -24,9 +24,7 @@ face_detector_kwargs = {
     "min_score_thresh" : 0.80,
     "min_suppression_threshold" : 0.3
 }
-# video: 50 frame 4-10  38 frame 15-20 31-35  37 frame 0-10  16 frame 0 - 4+ more
-# issue is we can get out ie two sets of landmarks for two different faces and no way to decipher
-# which is the right one + eg AFEW video 16 ground truths look at i believe the wrong face
+
 fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cpu',
                                   flip_input=False, face_detector='blazeface',
                                   face_detector_kwargs=face_detector_kwargs)
@@ -49,7 +47,9 @@ class AffectNet(Dataset):
         self.transform_image_shape = transform_image_shape
         self.transform_image = transform_image
         self.verbose = verbose
-
+           
+      ''' If do not have access to the json files below but do have access to the AffectNet 
+          ".npy" files then uncomment code below and ignore opening json file code'''
         # all_data = {}
         #
         # all_files = os.listdir(self.root_path.joinpath('images/'))
@@ -86,6 +86,8 @@ class AffectNet(Dataset):
         #
         #
 
+        
+        # json file contain landmarks detected in preprocessing
         if subset == 'train':
             file = '/vol/bitbucket/tg220/data/training_data_affectnet.json'
 
@@ -96,63 +98,42 @@ class AffectNet(Dataset):
         with open(file) as read_file:
             all_data = json.load(read_file)
 
+            
         print(len(all_data.keys()))
-
         self.all_data = all_data
-
         self.image_keys = list(self.all_data.keys())
 
-        self.frame_keys_subset = []
-
-        # 0:Neutral 1:Happy 2:Sad 3:Surprise 4:Fear 5:Disgust 6:Anger 7:Contempt
-        # (looking at the VA cirlce we really dont cover the bottom half very well at all)
-        self.expr_to_VA = {0: {'valence': 0, 'arousal': 0},
-                      1: {'valence': 0.9, 'arousal': 0.16},
-                      2: {'valence': -0.81, 'arousal': -0.4},
-                      3: {'valence': 0.42, 'arousal': 0.88},
-                      4: {'valence': -0.11, 'arousal': 0.79},
-                      5: {'valence': -0.67, 'arousal': 0.49},
-                      6: {'valence': -0.41, 'arousal': 0.78},
-                      7: {'valence': -0.57, 'arousal': 0.66}}
-
-       # self.frame_keys_subset = self.image_keys[130:210]
-       # print(len(self.frame_keys_subset))
-
+       
     def __len__(self):
         return len(self.image_keys)
-        # return len(self.frame_keys)
 
     def __getitem__(self, index):
-
+        
+        # set to True if no face is detected
         ignore_bounding_box = False
         bounding_box = None
         key = self.image_keys[index]
-        #print(key)
-
         sample_image = self.all_data[key]
 
 
         key = 'images/' + key
         image_file = self.image_path.joinpath(key).as_posix()
         image_file = image_file + '.jpg'
-        #print(image_file)
+        
 
         valence = torch.tensor([float(sample_image['valence'])], dtype=torch.float32)
         arousal = torch.tensor([float(sample_image['arousal'])], dtype=torch.float32)
         expression = torch.tensor([int(sample_image['expression'])], dtype=torch.long)
-
-        val_from_expr = torch.tensor([float(self.expr_to_VA[int(expression)]['valence'])], dtype=torch.float32)
-        aro_from_expr = torch.tensor([float(self.expr_to_VA[int(expression)]['arousal'])], dtype=torch.float32)
-
-
+        
+        ''' in Affectnet if -2 given as label it means label unknown therefore we give
+            this a value of 0'''
         if valence == -2:
             valence = 0
         if arousal == -2:
             arousal = 0
 
-        #gt_landmarks = sample_image['gt_landmarks']
+        # pre-loaded landmarks from json file where we did landmark detection in preprocessing
         predicted_landmarks = sample_image['my_landmarks']
-
         predicted_landmarks = np.array(predicted_landmarks)
 
         if len(predicted_landmarks.shape) > 2:
@@ -165,9 +146,9 @@ class AffectNet(Dataset):
         image = io.imread(image_file)
         image = np.ascontiguousarray(image)
 
-        #if ignore_bounding_box == False:
-        #    VR_dimension = [17, 9]
-        #    occluded_image = VR_patch(image, predicted_landmarks, VR_dim=VR_dimension)
+        if ignore_bounding_box == False:
+           VR_dimension = [20, 10]
+           occluded_image = VR_patch(image, predicted_landmarks, VR_dim=VR_dimension)
 
 
 
@@ -183,21 +164,18 @@ class AffectNet(Dataset):
                 image, landmarks = self.transform_image_shape(image, bb=bounding_box)
             # Fix for PyTorch currently not supporting negative stric
             image = np.ascontiguousarray(image)
-
+               
+           '''uncomment this code to see the output of the above operations'''
             #img = Image.fromarray(image, 'RGB')
             #img.show()
             #sys.exit()
-        ########################
-
-        #image = Image.fromarray(image, 'RGB')
+               
         
         if self.transform_image is not None:
             image = self.transform_image(image)
 
 
-
-        return dict(valence=valence, arousal=arousal, expression=expression, val_from_expr=val_from_expr,
-                    aro_from_expr=aro_from_expr, image=image, au=[])
+        return dict(valence=valence, arousal=arousal, expression=expression, image=image, au=[])
 
 
 
